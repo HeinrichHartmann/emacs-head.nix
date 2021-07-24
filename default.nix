b19d5e5530d660ef4d1fac0d5077a2b7cfd6f950
Heinrich Hartmann <heinrich.hartmann@zalando.de>
2021-07-24 14:19:23 +0000

    Add files from nixpkg

--- default.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> {});
-{
-  emacs27-nox = lowPrio (appendToName "nox" (emacs27.override {
-   withX = false;
-   withNS = false;
-   withGTK2 = false;
-   withGTK3 = false;
-  }));
-}
