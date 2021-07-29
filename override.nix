05fc042b4789e66218b40d58bfbec01ef80beff6
Heinrich Hartmann <derhein@gmail.com>
2021-07-29 19:18:07 +0000

    Add override.nix

---
+++ override.nix
@@ -0,0 +1,37 @@
+with (import <nixpkgs> {});
+
+{
+  emacs-gcc = (emacs-nox.overrideAttrs (old : {
+    pname = "emacs";
+    version = "head";
+    src = fetchFromGitHub {
+      owner = "emacs-mirror";
+      repo = "emacs";
+      rev = "4d439744685b6b2492685124994120ebd1fa4abb";
+      sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+    };
+    patches = [];
+    configureFlags = old.configureFlags ++ ["--with-json"];
+    preConfigure = "./autogen.sh";
+    buildInputs = old.buildInputs ++ [ autoconf texinfo ];
+  })).override {
+    nativeComp = true;
+  };
+
+  emacs-gcc-nox = (emacs-nox.overrideAttrs (old : {
+    pname = "emacs-nox";
+    version = "head";
+    src = fetchFromGitHub {
+      owner = "emacs-mirror";
+      repo = "emacs";
+      rev = "4d439744685b6b2492685124994120ebd1fa4abb";
+      sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+    };
+    patches = [];
+    configureFlags = old.configureFlags ++ ["--with-json"];
+    preConfigure = "./autogen.sh";
+    buildInputs = old.buildInputs ++ [ autoconf texinfo ];
+  })).override {
+    nativeComp = true;
+  };
+}
