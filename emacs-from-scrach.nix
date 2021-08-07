d4d0903a3072a84bd75113d3aca6aba4cf6b2a3d
Heinrich Hartmann <derhein@gmail.com>
2021-08-07 22:06:35 +0000

    emacs-from-scratch builds!

--- emacs-from-scrach.nix
+++ emacs-from-scrach.nix
@@ -3,5 +3,24 @@ with rec {
 };
 
 pkgs.stdenv.mkDerivation {
-
+  name = "emacs-head";
+  src = pkgs.fetchFromGitHub {
+      owner = "emacs-mirror";
+      repo = "emacs";
+      rev = "4d439744685b6b2492685124994120ebd1fa4abb";
+      sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
+  };
+  postPatch = pkgs.lib.concatStringsSep "\n" [
+    ''
+    substituteInPlace src/Makefile.in --replace 'RUN_TEMACS = ./temacs' 'RUN_TEMACS = env -i ./temacs'
+    substituteInPlace lisp/international/mule-cmds.el --replace /usr/share/locale ${pkgs.gettext}/share/locale
+    for makefile_in in $(find . -name Makefile.in -print); do
+      substituteInPlace $makefile_in --replace /bin/pwd pwd
+    done
+    ''
+  ];
+  preConfigure = "./autogen.sh";
+  configureFlags = ["--without-all"];
+  buildInputs = [ pkgs.autoconf pkgs.texinfo pkgs.ncurses ];
+  enableParallelBuilding = true;
 }
