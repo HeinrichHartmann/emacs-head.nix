a714aeec3ecb94dde0d445f8fd9554bce8f73247
Heinrich Hartmann <derhein@gmail.com>
2021-08-08 10:37:43 +0000

    Configure --with-native-compilation

--- emacs-from-scrach.nix
+++ emacs-from-scrach.nix
@@ -10,17 +10,26 @@ pkgs.stdenv.mkDerivation {
       rev = "4d439744685b6b2492685124994120ebd1fa4abb";
       sha256 = "00vxb83571r39r0dbzkr9agjfmqs929lhq9rwf8akvqghc412apf";
   };
-  buildInputs = [ pkgs.autoconf pkgs.texinfo pkgs.ncurses pkgs.gnutls pkgs.pkg-config pkgs.jansson ];
+  # for --native-compilation
+  NATIVE_FULL_AOT = "1";
+  LIBRARY_PATH = "${pkgs.stdenv.cc.libc}/lib";
+
+  buildInputs = [
+    pkgs.autoconf pkgs.texinfo pkgs.ncurses # --without-all
+    pkgs.gnutls pkgs.pkg-config # normal build
+    pkgs.jansson # --with-json
+    pkgs.zlib pkgs.libgccjit # --with-native-compilation
+  ];
+
   postPatch = pkgs.lib.concatStringsSep "\n" [
     ''
-    substituteInPlace src/Makefile.in --replace 'RUN_TEMACS = ./temacs' 'RUN_TEMACS = env -i ./temacs'
-    substituteInPlace lisp/international/mule-cmds.el --replace /usr/share/locale ${pkgs.gettext}/share/locale
     for makefile_in in $(find . -name Makefile.in -print); do
       substituteInPlace $makefile_in --replace /bin/pwd pwd
     done
+    substituteInPlace src/Makefile.in --replace 'RUN_TEMACS = ./temacs' 'RUN_TEMACS = env -i ./temacs'
+    substituteInPlace lisp/international/mule-cmds.el --replace /usr/share/locale ${pkgs.gettext}/share/locale
     ''
   ];
   preConfigure = "./autogen.sh";
-  configureFlags = [ "--with-json" ];
-  enableParallelBuilding = true;
+  configureFlags = [ "--with-json" "--with-native-compilation" ];
 }
